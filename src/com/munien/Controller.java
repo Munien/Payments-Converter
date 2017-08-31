8208215fc219a7ea4570c12ca162df965e99c7dc
Unknown <Dillon.munien@outlook.com>
2017-08-31 13:19:35 +1000

    Included Checking Input for Strings

--- src/com/munien/Controller.java
+++ src/com/munien/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package com.munien;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -15,11 +15,10 @@ public class Controller {
     @FXML private TextField converted;
     @FXML private ChoiceBox currency;
 
-    private final double monthtofort = 2.17262;
     NumberFormat formatter = new DecimalFormat("#0.00");
+    private final double monthtofort = 2.17262;
     private String currencyConvert;
-    private double currencyInt;
-    private double convertedInt;
+    private double currencyInt, convertedInt;
     private ArrayList<Character> characters = new ArrayList<Character>();
     private Alert alert = new Alert(Alert.AlertType.ERROR, "Cannot use text");
 
@@ -32,20 +31,29 @@ public class Controller {
         }
     }
 
+    public boolean testChar(char c) {
+        if(c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9')
+            return true;
+        else
+            return false;
+    }
+
     public boolean checkInput() {
         String text = convert.getText();
         boolean isNumber = true;
-//        for (int i = 0; i < text.length(); i++) {
-//            if (text.length() >= 1) {
-//                if(text.charAt(i) != '1' || text.charAt(i) != '2' || text.charAt(i) != '3' || text.charAt(i) != '4') {
-//                    System.out.println("Char at : " + text.charAt(i));
-//                    isNumber = false;
-//                    convert.clear();
-//                } else {
-//                    System.out.println("Char at : " + text.charAt(i));
-//                }
-//            }
-//        }
+        characters.clear();
+        for (int i = 0; i < text.length(); i++) {
+            characters.add(text.charAt(i));
+            for (char c : characters
+                 ) {
+                if(testChar(c) == false) {
+                    convert.clear();
+                    isNumber = false;
+                } else {
+                    isNumber = true;
+                }
+            }
+        }
         if(isNumber) {
             return true;
         } else {
--- src/com/munien/Main.java
+++ src/com/munien/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package com.munien;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Program.fxml"));
         primaryStage.setTitle("Month To Fornights Currency Converter");
         primaryStage.setScene(new Scene(root, 500, 275));
         primaryStage.show();
